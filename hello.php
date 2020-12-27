3eb0bf99f663046d5a7eb359d4dfa96c2f82441c
HaSaN <mahamudul951@gmail.com>
2020-12-27 12:05:14 +0600

    new line and variabol

--- hello.php
+++ hello.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <body>
-
+<h1> My First PHP Code</h1>
 <?php
 echo "Hello World!";
 ?>
---
+++ variabol.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1> My First PHP Code</h1>
+<?php
+    $t1 = 34;
+    $t2 = 56;
+    $t3 = $t1 + $t2;
+    $txt = "PHP";
+    //br is new line....
+    echo "I love $txt!<br>";
+    echo "sum : $t3"
+?>
+
+</body>
+</html>
