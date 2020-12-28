91cca534645372bbc2422df4e214a0af8c02095a
HaSaN <mahamudul951@gmail.com>
2020-12-28 12:40:36 +0600

    valid

--- index.php
+++ index.php
@@ -1,24 +1,53 @@
+
+
 <!DOCTYPE html>
 <html>
+
 <head> 
     <title> This is Title </title>
 </head>
 <body>
+<iframe src="http://www.atovait.com/"></iframe>
 
 <form action="welcome.php" method="post">
     Name : <input require type = "text" name = "username"/><br/><br/>
     Email : <input require type = "email" name = "email"/><br/><br/>
     
-    Gender: 
-    <select name="gender" require>
-        <option value = "">Select Gender</option>
-        <option value = "male">Male</option>
-        <option value = "female">Female</option>
+    Dristic: 
+    <select name="dristic" require>
+        <option value = "">Select Dristic</option>
+        <option value = "jamalpur">Jamalpur</option>
+        <option value = "dhaka">Dhaka</option>
+        <option value = "rongpur">Rongpur</option>
+        <option value = "tangail">Tangail</option>
     </select>
     <br><br>
+   
+
+    Gender:
+    <input type="radio" id="male" name="gender" value="male">
+    <label for="male">Male</label>
+    <input type="radio" id="female" name="gender" value="female">
+    <label for="female">Female</label>
+    <input type="radio" id="other" name="gender" value="other">
+    <label for="other">Other</label><br/><br/>
     <input type = "submit" value = "Submit">
+
+
 </form>
 
 
 </body>
 </html>
+<?php
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+    $name = $_POST['username'];
+    $email = $_POST['email'];
+    $gender = $_POST['gender'];
+    $dristic = $_POST['dristic'];
+    echo "User name : $name <br> Email :  $email <br>";
+    echo "Dristic : $dristic <br>";
+    echo "Gender is : $gender";
+    echo "<br>";
+}
+?>
--- welcome.php
+++ welcome.php
@@ -1,7 +1,19 @@
 <?php
-$name = $_POST['username'];
-$email = $_POST['email'];
-$gender = $_POST['gender'];
-echo "Welcome $name . your $email <br>";
-echo "GEnder is : $gender";
+
+function input ($data){
+    $data = trim($data);
+    $data = stripcslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+$name = input($_POST['username']);
+$email = input($_POST['email']);
+$gender =input( $_POST['gender']);
+$dristic =input( $_POST['dristic']);
+
+    echo "User name : $name <br> Email :  $email <br>";
+    echo "Dristic : $dristic <br>";
+    echo "Gender is : $gender";
+    echo "<br>";
 ?>
